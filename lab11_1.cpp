ee49778c0db4f468aeb65effcdb6f18a74c5f736
kob555 <chom.kappa@gmail.com>
2021-01-15 23:27:54 +0700

    fn

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,21 +1,21 @@
 #include<iostream>
 using namespace std;
 
-int fibonacci(int);
+long long int fibonacci(int);
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
+
+    cout << fibonacci(50);
+
     return 0; 
+
 }
 
-int fibonacci(int x){
+long long int fibonacci(int x){
     if(x==0) return 0;
     if(x==1) return 1;
     if(x>1){
         return fibonacci(x-1) + fibonacci(x-2);
     }
-    return 0;
 }
 
